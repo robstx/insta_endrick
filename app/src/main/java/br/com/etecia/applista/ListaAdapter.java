4bd424ba8d7bf84bca970a8ce27e61c22e389ab5
lndsilva <lndsilva@hotmail.com>
2024-03-12 19:17:49 -0300

    projeto lista livros

--- app/src/main/java/br/com/etecia/applista/ListaAdapter.java
+++ app/src/main/java/br/com/etecia/applista/ListaAdapter.java
@@ -1,4 +1,42 @@
 package br.com.etecia.applista;
 
-public class ListaAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ListaAdapter extends RecyclerView.Adapter<ListaAdapter.ViewHolder> {
+    private Context myContext;
+    private List<Livros> lstLivros;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstLivros.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView txtLivros;
+        ImageView imgLivros;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
